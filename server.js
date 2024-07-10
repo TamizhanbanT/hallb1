a7dc6b541707be150e81f52ac58f4167d4b9502d
TamizhanbanT <143093806+TamizhanbanT@users.noreply.github.com>
2024-07-10 11:23:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();});
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
